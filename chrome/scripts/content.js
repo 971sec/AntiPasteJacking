a5d2093314e8e3c38c7efda4e39e336023320ac3
chemouri <chemouri_2@yahoo.fr>
2016-05-31 12:35:05 +0000

    Add files via upload

---
+++ chrome/scripts/content.js
@@ -0,0 +1,58 @@
+/*
+  Copyright 2016 971sec <http://www.971sec.com/>
+
+  This program is free software: you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation, either version 3 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program. If not, see <https://www.gnu.org/licenses/>.
+
+  Authors (one per line):
+
+    Mohammed El Amin Chemouri <chemouri_2@yahoo.fr>
+*/
+var root = document.documentElement;
+var AntiPasteJacking = document.createElement('input');
+AntiPasteJacking.type = 'hidden';
+AntiPasteJacking.name = 'AntiPasteJacking';
+
+new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    if (mutation.attributeName == 'value')
+        chrome.runtime.sendMessage({AntiPasteJacking: true}, function() {
+          AntiPasteJacking.removeAttribute('value');
+        });
+  });
+}).observe(AntiPasteJacking, {attributes: true});
+
+(document.body || root).appendChild(AntiPasteJacking);
+var mock = document.createElement('script');
+mock.textContent =
+    'var eventListenerOrg = EventTarget.prototype.addEventListener; \n\
+var execCommandOrg = document.execCommand; \n\
+var AntiPasteJacking = document.getElementsByName(\'AntiPasteJacking\')[0]; \n\
+EventTarget.prototype.addEventListener = \n\
+    function(type, listener, useCapture) { \n\
+      if ((type.toUpperCase() == \'COPY\') || (type.toUpperCase() == \'CUT\')) \n\
+          eventListenerOrg.call(this, type, function() { \n\
+            AntiPasteJacking.value = true; \n\
+          }); \n\
+      else eventListenerOrg.call(this, type, listener, useCapture); \n\
+    } \n\
+document.execCommand = function(command, showUI, commandValue) { \n\
+var result = false;\n\
+if ((command.toUpperCase() == \'COPY\') || (command.toUpperCase() == \'CUT\')) {\n\
+	AntiPasteJacking.value = true; \n\
+} else { \n\
+    execCommandOrg.call(document, command, showUI, commandValue); \n\
+    result = true; \n\
+  } \n\
+  return result; \n\
+}';
+(document.head || root).appendChild(mock);
